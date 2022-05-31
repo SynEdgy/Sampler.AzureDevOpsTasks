0b2e58a9896a76b9f7a0089f550b750a67891b10
Johan Ljunggren <johan.ljunggren@shamn.se>
2022-05-31 21:53:19 +0200

    update task

--- source/tasks/Create_Changelog_PR.build.ps1
+++ source/tasks/Create_Changelog_PR.build.ps1
@@ -27,10 +27,9 @@
         The name of the default branch. Defaults to 'main'. It is used to compare
         and target the PR against.
 
-    .PARAMETER RepositoryPAT
-        The personal access token to use to access the Azure DevOps Git repository.
-        If left out the task assumes the authentication works without an personal
-        access token, e.g Windows integrated security.
+    .PARAMETER BasicAuthPAT
+        The personal access token to use to access the Azure DevOps Git repository
+        to create the PR.
 
     .PARAMETER BuildInfo
         The build info object from ModuleBuilder. Defaults to an empty hashtable.
@@ -69,7 +68,7 @@ param
     $MainGitBranch = (property MainGitBranch 'main'),
 
     [Parameter()]
-    $RepositoryPAT = (property RepositoryPAT ''),
+    $BasicAuthPAT = (property BasicAuthPAT ''),
 
     [Parameter()]
     $BuildInfo = (property BuildInfo @{ })
@@ -83,30 +82,24 @@ task Create_Changelog_PR {
 
     $branchName = "updateChangelogAfterv$ModuleVersion"
 
-    Write-Build DarkGray ("`tVerifying that changelog branch exist '{0}'." -f $branchName)
-
-    # $pullArguments = @()
-
-    # if ($RepositoryPAT)
-    # {
-    #     Write-Build DarkGray "`t`tUsing personal access token to pull commits and tags."
-
-    #     $patBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(('{0}:{1}' -f 'PAT', $RepositoryPAT)))
-
-    #     $pullArguments += @('-c', ('http.extraheader="AUTHORIZATION: basic {0}"' -f $patBase64))
-    # }
-
-    # # Track this branch on the remote 'origin
-    # $pullArguments += @('-c', 'http.sslbackend="schannel"', 'pull', 'origin', $MainGitBranch, '--tag')
-
-    # Sampler.AzureDevOpsTasks\Invoke-AzureDevOpsTasksGit -Argument $pullArguments
+    Write-Build DarkGray ("`tVerifying that changelog branch '{0}' exist." -f $branchName)
 
     # This should not use Invoke-SamplerGit as this should not throw if fails.
     $upstreamChangelogBranch = git ls-remote --heads origin $branchName
 
     if ($upstreamChangelogBranch)
     {
+        if ([System.String]::IsNullOrEmpty($BasicAuthPAT))
+        {
+            throw 'Must provide a personal access token to create a PR.'
+        }
+
         Write-Build DarkGray "`tCreating PR based on the changelog branch."
+
+        if ([System.String]::IsNullOrEmpty($RepositoryID))
+        {
+            $RepositoryID = $ProjectName
+        }
 
         <#
             TODO:
@@ -121,6 +114,33 @@ task Create_Changelog_PR {
             az repos pr create --detect true # maybe this works instead of above defaults?
             az repos pr create --repository-name $ProjectName --source-branch $branchName --target-branch $MainGitBranch --title "Updating Changelog since release of v$ModuleVersion" --description <description> --labels <label1> <label2> <label3>
         #>
+
+        #  +semver:skip
+
+        $payload = @{
+            sourceRefName = "refs/heads/$branchName"
+            targetRefName = "refs/heads/$MainGitBranch"
+            title = "Updating Changelog since release of v$ModuleVersion"
+            description = "Updating Changelog since release of v$ModuleVersion +semver:skip"
+        }
+
+        $patBase64 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(('{0}:{1}' -f 'PAT', $BasicAuthPAT)))
+
+        $invokeRestMethodParameters = @{
+            Method      = 'POST'
+            Uri         = "https://$Organization/$Collection/$Project/_apis/git/repositories/$RepositoryID/pullrequests?supportsIterations=false&api-version=6.0"
+            ContentType = 'application/json; charset=utf-8'
+            Headers     = @{
+                AUTHORIZATION = 'basic {0}' -f $patBase64
+            }
+
+            Body        = $payload | ConvertTo-Json
+            ErrorAction = 'Stop'
+        }
+
+        $result = Invoke-RestMethod @invokeRestMethodParameters
+
+        $result
 
         Write-Build Green ('Opened a PR for the changelog branch ''{0}''.' -f $branchName)
     }
